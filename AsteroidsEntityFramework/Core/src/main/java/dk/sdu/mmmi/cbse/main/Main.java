094891b065b293788aa2bc032115028c0a6b85ed
Sebjen0209 <70900449+Sebjen0209@users.noreply.github.com>
2023-05-16 11:34:17 +0200

    Tried ti implement spring...

--- AsteroidsEntityFramework/Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ AsteroidsEntityFramework/Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -1,5 +1,6 @@
 package dk.sdu.mmmi.cbse.main;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
@@ -12,7 +13,12 @@ public class Main {
 		config.setWindowSizeLimits(1200,700,1000,600);
 
 		new Lwjgl3Application(new Game(), config);
-		
+
+		AnnotationConfigApplicationContext application = new AnnotationConfigApplicationContext();
+		application.scan("dk.sdu.mmmi.cbse.main");
+		application.refresh();
+
+		new Lwjgl3Application((ApplicationListener) application.getBean("game"), cfg);
 	}
 	
 }
