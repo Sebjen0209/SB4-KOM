bab11e6e3f7af336e46d84c78289ab6bb489b6db
Sebjen0209 <70900449+Sebjen0209@users.noreply.github.com>
2023-05-17 16:59:05 +0200

    edited some pom files, since there were some errors

--- AsteroidsEntityFramework/Core/src/main/java/module-info.java
+++ AsteroidsEntityFramework/Core/src/main/java/module-info.java
@@ -6,9 +6,6 @@ module Core {
     requires java.desktop;
     requires com.badlogic.gdx;
     requires spring.context;
-    requires spring.core;
-    requires spring.beans;
-    opens dk.sdu.mmmi.cbse.main to spring.core;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
